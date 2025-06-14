3f4df5fa20ff7e1d39ebe5d8e59aff79f87c23fa
Supphakon <Hopper.mew@gmail.com>
2025-06-14 11:10:25 +0700

    setup projectgit

--- lib/main.dart
+++ lib/main.dart
@@ -38,12 +38,41 @@ void main() {
 
   
 }
-// Step 2: stateless widget
+// // Step 2: stateless widget
+// class Home extends StatelessWidget {
+//   const Home({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text("Hello Flutter");
+//   }
+// }
+
+//Step 3: container widget
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text("Hello Flutter");
+    //return Container(color: Colors.blueAccent, width: 300, height: 300);
+    //return Container(color: Colors.blueAccent, margin: EdgeInsets.all(10)); 
+    //return Container(color: Colors.blueAccent, width: 300, height: 300,);
+    //return Container(color: Colors.blueAccent, margin: EdgeInsets.all(10));
+    
+    return Center(
+      child: Container(
+        color: Colors.blueAccent,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        padding: EdgeInsets.all(10),
+        child: Text(
+          "Hello Flutter",
+          style: TextStyle(fontSize: 30, letterSpacing: 3),
+        ),
+      ),
+    );
   }
 }
+
+
+    
+    
