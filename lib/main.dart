1f9ead3947592294054b4ba2d9d3713a7035b74a
Supphakon <Hopper.mew@gmail.com>
2025-06-14 10:24:37 +0700

    setup projectgit

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,22 @@ void main() {
   //const app = MaterialApp(title: "My Title", home :Text("Hello World"));
   //runApp(app);
 
+  // runApp(
+  //   MaterialApp(
+  //     title: "My Title",
+  //     home: Scaffold(
+  //       appBar: AppBar(
+  //         title: Text("My App"),
+  //         backgroundColor: Colors.pinkAccent,
+  //         centerTitle: true,
+  //     ),
+  
+  //     )
+
+  //   )
+  // );
+
+  // Step 3: Scaffold widget
   runApp(
     MaterialApp(
       title: "My Title",
@@ -14,81 +30,20 @@ void main() {
           title: Text("My App"),
           backgroundColor: Colors.pinkAccent,
           centerTitle: true,
-      ),
-      
-      )
-
-    )
-  );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
- 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-    
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
- 
-    return Scaffold(
-      appBar: AppBar(
-     
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      
-        title: Text(widget.title),
-      ),
-      body: Center(
-   
-        child: Column(
-       
-       
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
         ),
+        body: const Home(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ),
+  );
+
+  
+}
+// Step 2: stateless widget
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello Flutter");
   }
 }
