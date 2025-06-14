6caba33375bf92379177c1ff35b4f9825e1ed5d3
Supphakon <Hopper.mew@gmail.com>
2025-06-14 13:59:05 +0700

    Step 5: container, color, main axis, cross axis

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -6,13 +6,73 @@ class Home extends StatelessWidget {
   // Step 5: container, color, main axis, cross axis
   @override 
   Widget build(BuildContext context) {
-    return Column(
+//     return Column(
+//     //mainAxisAlignment: MainAxisAlignment.start,
+//     //mainAxisAlignment: MainAxisAlignment.center,
+//     //mainAxisAlignment: MainAxisAlignment.end,
+//     //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//     //mainAxisAlignment: MainAxisAlignment.spaceAround,
+//     //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//     crossAxisAlignment: CrossAxisAlignment.stretch,
+
+//       children: [
+//         Container(
+//           color: Colors.orangeAccent,
+//           padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+//           child: Text(
+//             "Hello Flutter",
+//             style: TextStyle(
+//               fontSize: 20,
+//               letterSpacing: 3,
+//               color: Colors.white,
+//             ),
+//           ),
+//         ),
+//         Container(
+//           color: Colors.orangeAccent,
+//           padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+//           child: Text(
+//             "Hello Flutter",
+//             style: TextStyle(
+//               fontSize: 20,
+//               letterSpacing: 3,
+//               color: Colors.white,
+//             ),
+//           ),
+//         ),
+//         Container(
+//           color: Colors.orangeAccent,
+//           padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+//           child: Text(
+//             "Hello Flutter",
+//             style: TextStyle(
+//               fontSize: 20,
+//               letterSpacing: 3,
+//               color: Colors.white,
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
+return Row(
+    //mainAxisAlignment: MainAxisAlignment.start,
+    //mainAxisAlignment: MainAxisAlignment.center,
+    //mainAxisAlignment: MainAxisAlignment.end,
+    //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //mainAxisAlignment: MainAxisAlignment.spaceAround,
+    //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+
       children: [
         Container(
           color: Colors.orangeAccent,
           padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
           child: Text(
-            "Hello Flutter",
+            "Hello",
             style: TextStyle(
               fontSize: 20,
               letterSpacing: 3,
@@ -24,7 +84,7 @@ class Home extends StatelessWidget {
           color: Colors.orangeAccent,
           padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
           child: Text(
-            "Hello Flutter",
+            "Hello",
             style: TextStyle(
               fontSize: 20,
               letterSpacing: 3,
@@ -36,7 +96,7 @@ class Home extends StatelessWidget {
           color: Colors.orangeAccent,
           padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
           child: Text(
-            "Hello Flutter",
+            "Hello",
             style: TextStyle(
               fontSize: 20,
               letterSpacing: 3,
